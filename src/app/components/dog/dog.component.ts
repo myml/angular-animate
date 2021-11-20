cf69c2aa3f45ec06b5709a951a81b61aeda3a8a1
myml <wurongjie1@gmail.com>
2021-11-20 10:00:03 +0800

    feat: 添加dist文件

--- src/app/components/dog/dog.component.ts
+++ src/app/components/dog/dog.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { APP_BASE_HREF } from "@angular/common";
+import { Component, Inject, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-dog',
-  templateUrl: './dog.component.html',
-  styleUrls: ['./dog.component.scss']
+  selector: "app-dog",
+  templateUrl: "./dog.component.html",
+  styleUrls: ["./dog.component.scss"],
 })
 export class DogComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
   arr = new Array(50).fill(0);
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
